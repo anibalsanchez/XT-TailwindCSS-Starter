55355d6819826cd26b59cd3272cf5b7529aaddf7
anibalsanchez <anibal.sanchez@extly.com>
2019-06-09 12:30:44 +0200

    Update to mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -6,14 +6,10 @@
  * @see       https://www.extly.com
  */
 
-const path = require('path');
-const glob = require('glob');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const PATHS = {
-  src: path.join(__dirname, 'src'),
-};
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './src/styles.css',
@@ -21,22 +17,22 @@ module.exports = {
   module: {
     rules: [{
       test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
+      use: [
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            hmr: process.env.NODE_ENV === 'development',
           },
-          'postcss-loader',
-        ],
-      }),
+        },
+        'css-loader',
+        'postcss-loader',
+      ],
     }, ],
   },
   plugins: [
-    new ExtractTextPlugin('styles.css', {
-      disable: process.env.NODE_ENV === 'development',
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
     }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
