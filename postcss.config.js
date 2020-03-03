55af164d55b6453fc9828f07a00f85655aabbe7d
anibalsanchez <anibal.sanchez@extly.com>
2020-03-03 11:40:16 +0100

    refactor(copyright): Update copyright to 2020

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 /**
  * @author     Extly, CB <team@extly.com>
- * @copyright  Copyright (c)2007-2019 Extly, CB All rights reserved.
+ * @copyright  Copyright (c)2012-2020 Extly, CB All rights reserved.
  * @license    GNU General Public License version 3 or later; see LICENSE.txt
  *
  * @see       https://www.extly.com
